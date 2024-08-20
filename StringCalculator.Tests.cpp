0b119d60da6b1a47b71b390585bbbc4347ec7de0
nrithikraj <88530137+nrithikraj@users.noreply.github.com>
2024-08-20 15:24:34 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -21,13 +21,26 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_zero){
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
-TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_one){  //Arrange
+TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_one){  
+ //Arrange
   StringCalculator calculator;
   string input="1";
   int expectedsum=1;
   //Act
   int actualSum=calculator.Add(input);
   //Assert
-  ASSERT_EQ(actualSum,expectedsum);}
+  ASSERT_EQ(actualSum,expectedsum);
+}
+TEST(string_calculator_add_when_passed_two_comma_delimited_numbers,returns_their_sum){
+  //Arrange
+  StringCalculator calculator;
+  string input="1,2";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+ 
+}
 
 
