d6c36975cbcf769c93e0d468a028b72af4daac11
nrithikraj <88530137+nrithikraj@users.noreply.github.com>
2024-08-20 16:05:10 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -44,9 +44,7 @@ TEST(string_calculator_add_when_passed_two_comma_delimited_numbers,returns_their
 }
 TEST(string_calculator_addwhen_passed_negative_numbers,throws_an_exception_listing_invalid_values){
   StringCalculator calculator;
-  string input=""1,-2,-4,5";
-  //Ac
-  //Assert
+  string input="1,-2,-4,5";
   ASSERT_THROW(calculator.Add(input),invalid_argument);
  
 }
